a9aca82437ea089dcc7645fcd3019e3558cd4611
Muhammad <m.shaafan@outlook.de>
2025-04-03 18:31:28 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //notizen anzeigen lassen
 let notes = ['Banana', 'Rasen mähen'];
 
+let trashNotes = [];
+
 //wann werden sie angezeigt
 
 //wo werden sie angezeigt
@@ -14,13 +16,28 @@ function renderNotes() {
     }
 }
 
+function renderTrashNotes() {
+    let trashContentRef = document.getElementById('trash_content');
+    trashContentRef.innerHTML= "";
+
+    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
+        
+        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    }
+}
+
+
 function getNoteTemplate(indexNote) {
     return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>`;
 }
 
 
+function getTrashNoteTemplate(indexTrashNote) {
+    return `<p>+ ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">x</button></p>`;
+}
+
 function addNote() {
-    let noteInputRef = document.getElementById('note-input');
+    let noteInputRef = document.getElementById('note_input');
     let noteInput = noteInputRef.value;
     notes.push(noteInput);
     renderNotes();
@@ -28,6 +45,8 @@ function addNote() {
 }
 
 function deleteNote(indexNote) {
-    notes.splice(indexNote, 1);
+    let trashNote = notes.splice(indexNote, 1);
+    trashNotes.push(trashNote);
     renderNotes()
+    renderTrashNotes(); 
 }
