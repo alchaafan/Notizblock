e8d00726f78ad58bf4a8d1e30a43b4d9216ac3b5
Muhammad <m.shaafan@outlook.de>
2025-04-07 15:54:21 +0200

    mit Papierkorb

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let notes = ["Arbeit", "Auto", "Wohnung"];
 
+let trashNotes = [];
+
 function renderNotes() {
     let contentRef = document.getElementById('content')
    contentRef.innerHTML = "";
@@ -11,8 +13,26 @@ function renderNotes() {
     }
 }
 
+
+function renderTrashNotes() {
+    let trashContentRef = document.getElementById('trash_content')
+    trashContentRef.innerHTML = "";
+
+
+    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
+        
+        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    }
+}
+
+
+
 function getNoteTemplate(indexNote) {
     return `<p>${notes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>` 
+}
+
+function getTrashNoteTemplate(indexTrashNote) {
+    return `<p>${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">x</button></p>` 
 }
 
 function addNote() {
@@ -23,6 +43,8 @@ function addNote() {
 }
 
 function deleteNote(indexNote) {
-    notes.splice(indexNote,1)
+    let trashNote = notes.splice(indexNote,1)
+    trashNotes.push(trashNote);
     renderNotes();
+    renderTrashNotes();
 }
