7786d08389a7c4844717249d83e91fcd53dcf04f
Muhammad <m.shaafan@outlook.de>
2025-04-07 13:08:44 +0200

    neu starten

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
-//notizen anzeigen lassen
-let notes = ['Banana', 'Rasen mähen'];
+let notes = ["Auto", "Arbeit", "Wohnung"];
 
-let trashNotes = [];
-
-//wann werden sie angezeigt
-
-//wo werden sie angezeigt
 function renderNotes() {
-    let contentRef = document.getElementById('content');
-    contentRef.innerHTML= "";
+    let contentRef = document.getElementById('content')
+   contentRef.innerHTML = "";
+
 
     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        
-        contentRef.innerHTML += getNoteTemplate(indexNote);
+        const note = notes[indexNote];
+        contentRef.innerHTML += getNoteTemplate(note);
     }
 }
 
-function renderTrashNotes() {
-    let trashContentRef = document.getElementById('trash_content');
-    trashContentRef.innerHTML= "";
-
-    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
-        
-        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
-    }
-}
-
-
-function getNoteTemplate(indexNote) {
-    return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>`;
-}
-
-
-function getTrashNoteTemplate(indexTrashNote) {
-    return `<p>+ ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">x</button></p>`;
+function getNoteTemplate(note) {
+    return `<p>${note}</p>`
 }
 
 function addNote() {
@@ -41,12 +20,4 @@ function addNote() {
     let noteInput = noteInputRef.value;
     notes.push(noteInput);
     renderNotes();
-    noteInputRef.value = "";
-}
-
-function deleteNote(indexNote) {
-    let trashNote = notes.splice(indexNote, 1);
-    trashNotes.push(trashNote);
-    renderNotes()
-    renderTrashNotes(); 
 }
